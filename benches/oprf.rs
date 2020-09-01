cdf0c7bf72be5408a64fb9580472eb25ac74aaf9
Kevin Lewi <klewi@cs.stanford.edu>
2020-09-01 10:23:39 -0700

    Update copyright in benches/oprf.rs

--- benches/oprf.rs
+++ benches/oprf.rs
@@ -1,5 +1,7 @@
-// Copyright (c) The Libra Core Contributors
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
 
 #[macro_use]
 extern crate criterion;
