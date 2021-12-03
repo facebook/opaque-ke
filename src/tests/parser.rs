adbd50f5239b2e23716c4280c2383f7dcaa1045c
Kevin Lewi <klewi@fb.com>
2021-12-03 14:38:11 -0800

    Updating to dual-license (#252)

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -1,7 +1,9 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
 //
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
+// This source code is licensed under both the MIT license found in the
+// LICENSE-MIT file in the root directory of this source tree and the Apache
+// License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+// of this source tree.
 
 use alloc::string::String;
 
