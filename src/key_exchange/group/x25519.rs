32ed3e677a4fccef8b06d5fa7503665824ecc78d
dAxpeDDa <daxpedda@gmail.com>
2022-02-25 17:14:25 +0100

    Base `X25519` implementation on `curve25519-dalek`

--- src/key_exchange/group/x25519.rs
+++ src/key_exchange/group/x25519.rs
@@ -7,14 +7,16 @@
 
 //! Key Exchange group implementation for X25519
 
-use curve25519_dalek_3::scalar::Scalar;
+use curve25519_dalek::constants::ED25519_BASEPOINT_TABLE;
+use curve25519_dalek::montgomery::MontgomeryPoint;
+use curve25519_dalek::scalar::Scalar;
+use curve25519_dalek::traits::Identity;
 use digest::core_api::BlockSizeUser;
 use digest::Digest;
 use elliptic_curve::hash2curve::{ExpandMsg, ExpandMsgXmd, Expander};
 use generic_array::typenum::{IsLess, IsLessOrEqual, U256, U32, U64};
 use generic_array::GenericArray;
 use rand::{CryptoRng, RngCore};
-use x25519_dalek::{PublicKey, StaticSecret};
 
 use super::KeGroup;
 use crate::errors::InternalError;
@@ -24,9 +26,9 @@ pub struct X25519;
 
 /// The implementation of such a subgroup for Ristretto
 impl KeGroup for X25519 {
-    type Pk = PublicKey;
+    type Pk = MontgomeryPoint;
     type PkLen = U32;
-    type Sk = StaticSecret;
+    type Sk = Scalar;
     type SkLen = U32;
 
     fn serialize_pk(pk: &Self::Pk) -> GenericArray<u8, Self::PkLen> {
@@ -34,21 +36,21 @@ impl KeGroup for X25519 {
     }
 
     fn deserialize_pk(bytes: &GenericArray<u8, Self::PkLen>) -> Result<Self::Pk, InternalError> {
-        if **bytes == [0; 32] {
+        let pk = MontgomeryPoint((*bytes).into());
+
+        if pk == MontgomeryPoint::identity() {
             Err(InternalError::PointError)
         } else {
-            Ok(PublicKey::from(<[_; 32]>::from(*bytes)))
+            Ok(pk)
         }
     }
 
     fn random_sk<R: RngCore + CryptoRng>(rng: &mut R) -> Self::Sk {
-        let mut scalar_bytes = [0u8; 32];
-
         loop {
-            rng.fill_bytes(&mut scalar_bytes);
+            let scalar = Scalar::random(rng);
 
-            if scalar_bytes != [0u8; 32] {
-                break StaticSecret::from(scalar_bytes);
+            if scalar != Scalar::zero() {
+                break scalar;
             }
         }
     }
@@ -65,17 +67,15 @@ impl KeGroup for X25519 {
             .map_err(|_| InternalError::HashToScalar)?
             .fill_bytes(&mut uniform_bytes);
 
-        Ok(StaticSecret::from(
-            Scalar::from_bytes_mod_order_wide(&uniform_bytes.into()).to_bytes(),
-        ))
+        Ok(Scalar::from_bytes_mod_order_wide(&uniform_bytes.into()))
     }
 
     fn public_key(sk: &Self::Sk) -> Self::Pk {
-        PublicKey::from(sk)
+        (&ED25519_BASEPOINT_TABLE * sk).to_montgomery()
     }
 
     fn diffie_hellman(pk: &Self::Pk, sk: &Self::Sk) -> GenericArray<u8, Self::PkLen> {
-        sk.diffie_hellman(pk).to_bytes().into()
+        Self::serialize_pk(&(sk * pk))
     }
 
     fn serialize_sk(sk: &Self::Sk) -> GenericArray<u8, Self::SkLen> {
@@ -83,16 +83,8 @@ impl KeGroup for X25519 {
     }
 
     fn deserialize_sk(bytes: &GenericArray<u8, Self::PkLen>) -> Result<Self::Sk, InternalError> {
-        if **bytes == [0; 32] {
-            Err(InternalError::PointError)
-        } else {
-            let sk = StaticSecret::from(<[u8; 32]>::from(*bytes));
-
-            if sk.to_bytes() == **bytes {
-                Ok(sk)
-            } else {
-                Err(InternalError::PointError)
-            }
-        }
+        Scalar::from_canonical_bytes((*bytes).into())
+            .filter(|scalar| scalar != &Scalar::zero())
+            .ok_or(InternalError::PointError)
     }
 }
